f3d78ad187fa21f3e3483d53974d184c321bad7e
Hubaib19 <hubaib0019@gmail.com>
2023-12-29 10:56:55 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:exapro/view/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (context) => ContProvider(),)
-    ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ContProvider(),
+        )
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
